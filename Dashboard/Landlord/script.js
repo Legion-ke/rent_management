a8560125d05cf7a0d8013b63862c469003e09497
Given Omondi <70665077+Legion-ke@users.noreply.github.com>
2024-03-28 13:48:05 +0300

    Delete Dashboard directory

--- Dashboard/Landlord/script.js
+++
@@ -1,7 +0,0 @@
-function logout() {
-    // Redirect to a specified location (replace 'your_logout_page.html' with the actual URL)
-    window.location.href = '/Dashboard-login/signin.html';
-
-    // Return a message indicating successful logout
-    return 'Successfully logged out.';
-}
